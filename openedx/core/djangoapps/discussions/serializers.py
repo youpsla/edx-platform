dc490ad59706b8151f3318cda34c93f93cab17fe
Jawayria <jawayriahashmi@gmail.com>
2021-11-25 12:06:32 +0500

    fix: correction

--- openedx/core/djangoapps/discussions/serializers.py
+++ openedx/core/djangoapps/discussions/serializers.py
@@ -10,7 +10,7 @@ from openedx.core.djangoapps.django_comment_common.models import CourseDiscussio
 from openedx.core.lib.courses import get_course_by_id
 from xmodule.modulestore.django import modulestore  # lint-amnesty, pylint: disable=wrong-import-order
 from .models import AVAILABLE_PROVIDER_MAP, DEFAULT_PROVIDER_TYPE, DiscussionsConfiguration, Features, Provider
-rom .utils import available_division_schemes, get_divided_discussions
+from .utils import available_division_schemes, get_divided_discussions
 
 
 class LtiSerializer(serializers.ModelSerializer):
